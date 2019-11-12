0e44cde8e36a86606bdeb70fe47d51ae3adc669d
Lynn Qi qilin2481@gmail.com <qilin2481@gmail.com>
2019-11-12 14:13:26 -0500

    list the tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index', as: 'tasks'
+  get 'tasks/:id', to: 'tasks#show', as: 'show_tasks'
 end
